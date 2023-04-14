eeadfa0af45eaea0b8a212a51bf31a2dba5ec10b
PelsonBreak <damianpelczar10@gmail.com>
2023-04-14 13:45:59 +0200

    Zadanie dzien3

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.Design;
+
+var name1 = ("Ewa");
+var gender1 = ("female");
+var age1 = 29;
+var age2 = 33;
+
+if (gender1 == "female" && age1 < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else
+{
+    Console.WriteLine("Kobieta powyżej 30 lat");
+}
+
+if (name1 == "Ewa"  && gender1 == "female" && age2 > 32)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else
+{
+    Console.WriteLine("Nie jestem Ewa");
+}
+
+string name2 = ("Adam");
+string gender2 = ("male");
+int age3 = 17;
+
+if (name2 == "Adam" && gender2 == "male" && age3 < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczynza");
+}
+else
+{
+    Console.WriteLine("Nie jestem Adam");
+}
+
+if (gender2 == "male")
+{
+    Console.WriteLine("Adam jest mężczyzną");
+}
+else
+{
+    Console.WriteLine("Adam jest kobietą");
+}
+if (!(gender2 == "male"))
+{
+    Console.WriteLine("Adam jest mężczyzną");
+}
+else
+{
+    Console.WriteLine("Adam jest kobietą");
+}
